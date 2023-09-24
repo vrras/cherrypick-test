b264992e9c684420f07ec772f9f0f8d6f48a5731
Firas Luthfi <firas.luthfi@moladin.com>
2023-09-24 21:15:12 +0700

    add new route version

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ app.route('/')
     res.send('Hello World!');
   });
 
+  app.route('/version')
+  .get((req, res) => {
+    res.status(200).json({
+      serviceName: 'cherrypick-service',
+      status: 'OK',
+      version: 'Version 1'
+    });
+  });
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
